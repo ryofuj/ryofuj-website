11adb0c4d3432d17a5ffec59d9e8dfd81869e031
Ryo Fujimura <36524811+ryofujimura@users.noreply.github.com>
2025-01-04 09:23:44 +0000

    organize file into sections

--- app.py
+++ app.py
@@ -6,17 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/portfolio')
-def portfolio():
-    return render_template('portfolio.html')
-
-@app.route('/search_filter')
-def search_filter():
-    return render_template('search_filter.html')
-
-@app.route('/experience')
-def experience():
-    return render_template('experience.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
