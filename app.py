a228c88cfd0d1429c815bfd75787f268f23b34f7
Ryo Fujimura <ryo176999@gmail.com>
2025-01-04 19:08:16 -0800

    minor update on experience

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
